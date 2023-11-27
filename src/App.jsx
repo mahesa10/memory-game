e2c3bc4480a7781658d118bc434a413617eef5e8
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-11-27 07:30:59 +0700

    Add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './Header'
+import Card from './Card'
 import './App.css'
 
 function App() {
   return (
     <div id="wrapper">
       <Header></Header>
+      <Card></Card>
     </div>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+import './Card.css'
+
+function Card() {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <div className="country-flag"></div>
+        <p className='country-name'>Country Name</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
