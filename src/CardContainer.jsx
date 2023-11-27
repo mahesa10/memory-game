48899ba341dfd0541b042db9066146f71d7d1b49
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-11-27 09:58:00 +0700

    Add card container style

--- src/CardContainer.jsx
+++ src/CardContainer.jsx
@@ -1,8 +1,16 @@
 import Card from "./Card"
+import "./styles/CardContainer.css"
 
 function CardContainer() {
   return (
     <div className="card-container">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
       <Card></Card>
     </div>
   )
